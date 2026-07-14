76b26ad204677f74e7eae5cc78f8de646202f315
Johannes Reim <reim.j82@gmail.com>
2026-07-14 13:07:16 +0200

    deployment test

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ app = FastAPI()
 def root():
     return {"message": "Welcome!"}
 
+@app.get("/recipes/{recipe_name}")
+def get_recipe(recipe_name):
+    session = Session(DatabaseConnection())
+
+    repo = RecipeRepository(session)
+
+    recipe = repo.get_by_name(recipe_name)
+
+    if recipe is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Recipe not found"
+        )
+
+    return recipe
